1064ba8713f9466811a261bcfa16131ed275e58b
Ehsan <mhdmkhor@gmail.com>
2017-06-30 15:58:22 +0430

    try to customize examples/custom_animate.py6

--- examples/shapes.py
+++ examples/shapes.py
@@ -94,7 +94,7 @@ font = ImageFont.load_default()
 # font = ImageFont.truetype('Minecraftia.ttf', 8)
 
 # Write some text.
-draw.text((1,1), 'Hello World!', font=font)
+draw.text((1,1), u"\u2605" + u"\u2606" + u"Текст на русском", font=font)
 
 # Display image.
 disp.image(image)
