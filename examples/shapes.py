0767251efb2f3ebd187303c829d1e48d386ceefe
Ehsan <mhdmkhor@gmail.com>
2017-06-30 15:52:18 +0430

    try to customize examples/custom_animate.py

--- examples/shapes.py
+++ examples/shapes.py
@@ -83,8 +83,8 @@ draw = ImageDraw.Draw(image)
 # draw.ellipse((2,2,22,22), outline=0, fill=255)
 # draw.rectangle((24,2,44,22), outline=0, fill=255)
 # draw.polygon([(46,22), (56,2), (66,22)], outline=0, fill=255)
-# draw.line((68,22,81,2), fill=0)
-# draw.line((68,2,81,22), fill=0)
+draw.line((68,22,81,2), fill=0)
+draw.line((68,2,81,22), fill=0)
 
 # Load default font.
 font = ImageFont.load_default()
